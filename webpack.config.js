10b377bcd758708e7714a3f811e773a63834550a
Benoit Podwinski <me@benoitpodwinski.com>
2020-04-30 16:52:51 +0200

    add fontawesome at webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const webpack = require('webpack');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 let sourceMap = true;
+let outputStyle = "expanded";
 
 if (process.env.NODE_ENV === 'production') {
   sourceMap = false;
+  outputStyle = "compressed";
 }
-
+ 
 module.exports = {
   mode: "development",
   entry: {
@@ -25,42 +27,54 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../",
-            },
-          },
-          {
-            loader: "css-loader",
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-      {
-        test: /\.jpe?g$|\.gif$|\.png$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[path][name].[ext]",
-              outputPath: "../",
-            },
-          },
-        ],
-      },
-      {
         test: /\.(svg|eot|woff|woff2|ttf)$/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "[path][name].[ext]",
-              outputPath: "../",
+              outputPath: "./assets/fonts/",
+              publicPath: "../fonts/",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "./assets/img/",
+              publicPath: "../img/",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: sourceMap,
+              url: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: sourceMap,
+              sassOptions: {
+                outputStyle: outputStyle,
+              },
             },
           },
         ],
